45cf49cc862b92bd2997ebc272cf9cd7ae2d160b
Sean J. Taylor <sjt@fb.com>
2014-11-15 13:00:35 -0800

    Preliminary version of the server

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-.PHONY: data
+.PHONY: data deps
+
+deps:
+	pip install -r requirements.txt
 
 data: data/calls.csv data/incidents.csv data/sfpd_service_calls.csv
 
@@ -10,3 +13,4 @@ data/incidents.csv:
 
 data/sfpd_service_calls.csv:
 	wget https://s3-us-west-1.amazonaws.com/acs-sfpd-data/sfpd_service_calls.csv
+
---
+++ app/server.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import json
+
+from geojson import MultiPoint, Point, Polygon, MultiPolygon, GeometryCollection
+
+from tornado.web import RequestHandler, StaticFileHandler
+from tornado.httpserver import HTTPServer
+from tornado.gen import coroutine
+
+class BaseHandler(RequestHandler):
+    def initialize(self, db=None):
+        self.db = db
+
+    def get_current_user(self):
+        user_id = self.get_secure_cookie('userid')
+        #if not user_id:
+        #    token = self.get_argument('access_token', None)
+        return user_id
+
+    def get_static_url(self, path):
+        return '%s://%s%s' % (self.request.protocol, self.request.host, path)
+    
+    def write_json(self, d):
+        self.write(json.dumps(d))
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        return self.render('index.html')
+
+
+class HeatMapHandler(BaseHandler):
+    def get(self):
+        crime_type = self.get_query_argument('crime_type', 'theft')
+        time_of_day = self.get_query_argument('tod', 'evening')
+        day_of_week = self.get_query_argument('dow', 'weekend')
+
+        geo = MultiPoint([(-155.52, 19.61), (-156.22, 20.74), (-157.97, 21.46)])
+        return self.write_json(geo)
+
+if __name__ == '__main__':
+    from tornado.options import define, options
+    from tornado.web import Application
+    from tornado.ioloop import IOLoop
+
+    define('conf', default='app/config.json', help='Configuration file')
+
+    options.parse_command_line()
+    with open(options.conf) as f:
+        config = json.load(f)
+
+
+    config['static_path'] = os.path.join(
+        os.path.abspath(os.path.curdir),
+        config['static_path']
+    )
+    app = Application([
+        (r'/', MainHandler),
+        (r'/api/heatmap.json', HeatMapHandler),
+        (r'/((?:css|fonts|js)/.*)', StaticFileHandler, {'path': config['static_path']}),
+    ], **config)
+
+    app.listen(config['port'])
+    IOLoop.instance().start()
