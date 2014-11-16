e8431ab0c81ede683438a771a1ec7cf3767b95f0
tvirot <virot@fb.com>
2014-11-16 04:12:48 -0800

    colorize heatmap

--- www/js/out.for.justice.js
+++ www/js/out.for.justice.js
@@ -1,4 +1,5 @@
 var HEATMAP_API = 'api/heatmap.json'
+var HEATMAP_COLOR = ["#f7f4f9","#e7e1ef","#d4b9da","#c994c7","#df65b0","#e7298a","#ce1256","#980043","#67001f"];
 
 function expandMap() {
   // Expand map to 100% height
@@ -23,9 +24,9 @@ $(window).resize(expandMap);
 
 // Init map
 var map = L.map('map', {
-  center: [37.788975, -122.403452],
-  zoom: 14,
-  minZoom: 12,
+  center: [37.767254489700846, -122.439],
+  zoom: 13,
+  minZoom: 13,
   layers: new L.StamenTileLayer('toner-lite'),
   scrollWheelZoom: false,
   zoomControl: false
@@ -83,19 +84,45 @@ function alignSVG() {
   }
 }
 
-function renderPoints(dat, target, classStr) {
+function renderPoints(dat, type, target, classStr) {
   target.selectAll('.' + classStr).remove();
 
+  var newData = dat.map(function(d) {
+    var value;
+    if (type === 'intox') {
+      value = d.intoxication;
+    } else if (type === 'property') {
+      value = d.property;
+    } else {
+      value = d.violent;
+    }
+
+    return {
+      point: map.latLngToLayerPoint([d.coordinates[0], d.coordinates[1]]),
+      value: value
+    };
+  })
+
+  newData = newData.filter(function(d) { return d.value > 2.5; })
+
+  // Move this outside. Do it once, and cache it!
+  var quantile = d3.scale.quantile()
+    .domain(d3.extent(newData, function(d) { return d.value; }))
+    .range([2, 3, 4, 5, 6, 7, 8]);
+  // var color = d3.scale.log()
+  //   .domain([1, 6])
+  //   .range(['#6ECFF5', '#D95C5C']);
+
   var points = target.selectAll('.' + classStr)
-    .data(dat.map(function(d) {
-      return map.latLngToLayerPoint([d.coordinates[0], d.coordinates[1]]);
-    }));
+    .data(newData);
 
   points.enter().append('circle')
     .attr('class', classStr)
     .attr('r', map.getZoom() / 3.0)
-    .attr('cx', function(d) { return d.x; })
-    .attr('cy', function(d) { return d.y; });
+    .attr('cx', function(d) { return d.point.x; })
+    .attr('cy', function(d) { return d.point.y; })
+    .style('fill', function(d) { return HEATMAP_COLOR[quantile(d.value)]; })
+    .style('opacity', function(d) { return quantile(d.value) < 3 ? 0.25 : 0.8; });
 }
 
 function renderCars(dat) {
@@ -136,7 +163,7 @@ function renderCarIcon(e) {
 }
 
 function update() {
-  renderPoints(data, layers.heatmap, 'point');
+  renderPoints(nodes, selectedType, layers.heatmap, 'point');
   renderCars(cars);
 }
 
@@ -159,16 +186,15 @@ function step() {
 
 /* TEMP */
 
-var data;
+var nodes;
+var selectedType = 'property';
 var cars = [[37.776317,-122.395569], [37.7483385,-122.4079037]];
 
 d3.json(HEATMAP_API, function(error, json){
-  data = json.geometries;
-  var latExtent = d3.extent(data, function(d) { return d.coordinates[0]; });
-  var lonExtent = d3.extent(data, function(d) { return d.coordinates[1]; });
+  nodes = json.geometries;
 
   init();
-  renderPoints(data, layers.heatmap, 'point');
+  renderPoints(nodes, selectedType, layers.heatmap, 'point');
   renderCars(cars);
 });
 
